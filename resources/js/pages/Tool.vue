244916841ac13eae09d4d82ba34e70f2201aaa50
Mior Muhammad Zaki <crynobone@gmail.com>
2025-04-15 10:01:38 +0800

    Supports Laravel Nova 5.5
    Signed-off-by: Mior Muhammad Zaki <crynobone@gmail.com>

--- resources/js/pages/Tool.vue
+++ resources/js/pages/Tool.vue
@@ -75,7 +75,7 @@
 import CodeMirror from 'codemirror'
 import ToolbarButton from '../components/ToolbarButton.vue'
 import { singularOrPlural } from 'laravel-nova-util'
-import isNil from 'lodash/isNil'
+import { isNil } from 'es-toolkit/compat'
 
 export default {
   codemirror: null,
